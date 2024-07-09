719eb32acc3c2be7b4fe88e0f8252a4ec97217b1
dwhitejr3 <dwhitejr3@gmail.com>
2024-07-08 19:29:02 -0500

    more setup

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,15 @@
 import { Text, View } from "react-native";
+import { Link } from "expo-router";
+import { StatusBar } from "expo-status-bar";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text>YOOOOOOOOO</Text>
+      <Link href="/profile" style={{ color: "pink" }}>
+        {" "}
+        Go to profile{" "}
+      </Link>
     </View>
   );
 }
